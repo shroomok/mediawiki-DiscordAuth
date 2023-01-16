33f80de3d99f696163c95f3ef1d228ea15078af0
Shroomok <shroomok@gmail.com>
2023-01-16 23:21:38 +0700

    Discord RestCord library Dependency Injection

---
+++ includes/ServiceWiring.php
@@ -0,0 +1,12 @@
+<?php
+
+use MediaWiki\MediaWikiServices;
+use RestCord\DiscordClient;
+
+/** @phpcs-require-sorted-array */
+return [
+    'DiscordClient' => function ( MediaWikiServices $services ) {
+        $botToken = $services->getMainConfig()->get('DiscordAuthBotToken');
+        return new DiscordClient( ['token' => $botToken] );
+    },
+];
